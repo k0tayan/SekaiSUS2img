1be3bc333193fc46724301b16913f9939a543fea
k0tayan <kotayan0415@outlook.jp>
2022-12-17 14:06:33 +0900

    Refactor landscape.js

--- public/landscape.js
+++ public/landscape.js
@@ -1,19 +1,19 @@
-window.onload = ()=>{
+window.onload = () => {
     const canvas = document.getElementById('chart-landscape');
     const ctx = canvas.getContext("2d");
     const chartImage = new Image();
     chartImage.src = "data:image/png;base64," + pngString;
-    chartImage.onload = ()=>{
+    chartImage.onload = () => {
         const width = chartImage.width;
         const height = chartImage.height;
-        const m = Math.floor(height/ (pixelsPerBeat * 8));
+        const m = Math.floor(height / (pixelsPerBeat * 8));
         let totalLeft = 0;
-        canvas.width = width * (m+1);
+        canvas.width = width * (m + 1);
         for (let i = 0; i < m; i++) {
             const top = height - (i + 1) * pixelsPerBeat * 8;
             const left = totalLeft;
             totalLeft += width;
-            ctx.drawImage(chartImage, 0, top, width, pixelsPerBeat *8, left, 0, width, pixelsPerBeat * 8);
+            ctx.drawImage(chartImage, 0, top, width, pixelsPerBeat * 8, left, 0, width, pixelsPerBeat * 8);
         }
         ctx.drawImage(chartImage, 0, 0, width, height % (pixelsPerBeat * 8), totalLeft, pixelsPerBeat * 8 - height % (pixelsPerBeat * 8), width, height % (pixelsPerBeat * 8));
     }
